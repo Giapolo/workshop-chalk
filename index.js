c88d24bc7a5bac9cc1ca9fae93eccb30ad6fa74f
Maxime Giampaoli <maxime.giampaoli@gmail.com>
2025-03-25 11:29:23 +0100

    ADD: wilders workshop

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+const names = ["eric", "messica", "dara", "maxime"]
+console.log(chalk.blue(names[0]));
+console.log(chalk.red(names[1]));
+console.log(chalk.green(names[2]));
+console.log(chalk.yellow(names[3]));
